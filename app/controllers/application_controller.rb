7e0b60e0cf708a10375e6bddde0eac6f0804435a
ubuntu <you@example.com>
2022-04-08 10:55:12 +0530

    add application file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
 	# def show_employee
 	# end
 
-	def after_sign_in_path_for(resource_or_scope)
-		if request.ip != "106.215.64.138"
-	 		destroy_employee_session_path
-	 	else
-        	stored_location_for(resource_or_scope) || signed_in_root_path(resource_or_scope)
-	 	end
-      end
+	# def after_sign_in_path_for(resource_or_scope)
+	# 	if request.ip != "106.215.64.138"
+	#  		destroy_employee_session_path
+	#  	else
+ #        	stored_location_for(resource_or_scope) || signed_in_root_path(resource_or_scope)
+	#  	end
+ #      end
 end
