b2249f5e2c5815d577f274385e3aa22bfe79c2d5
ubuntu <you@example.com>
2022-04-08 14:49:19 +0530

    change ip address

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,12 @@ class ApplicationController < ActionController::Base
 	# def show_employee
 	# end
 
-	def after_sign_in_path_for(resource_or_scope)
-		ip = Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
-		if request.remote_ip != ip
-	 		destroy_employee_session_path
-	 	else
-        	stored_location_for(resource_or_scope) || signed_in_root_path(resource_or_scope)
-	 	end
-      end
+	# def after_sign_in_path_for(resource_or_scope)
+	# 	#Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
+	# 	if request.remote_ip != "127.0.0.1"
+	#  		destroy_employee_session_path
+	#  	else
+ #        	stored_location_for(resource_or_scope) || signed_in_root_path(resource_or_scope)
+	#  	end
+ #    end
 end
