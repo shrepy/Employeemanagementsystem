1a330c9ce1261b98b9faf368f321a0f0c96e6758
ubuntu <you@example.com>
2022-05-09 18:38:25 +0530

    Remove space

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,6 @@ class ApplicationController < ActionController::Base
   #    	end
   #    end
 
-
   rescue_from CanCan::AccessDenied do
     flash[:error] = 'Access denied!'
     redirect_to root_url
