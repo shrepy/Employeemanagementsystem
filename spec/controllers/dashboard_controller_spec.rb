693859d1226e1113888ee3ce24b654437d8e57a5
ubuntu <you@example.com>
2022-05-13 16:50:44 +0530

    Recent withing 3 days joined employee should show in dashboard

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -9,7 +9,8 @@ RSpec.describe DashboardController, type: :controller do
     FactoryBot.create(:employee, role_id: role.id, designation_id: designation.id, date_of_birth: Time.now.to_date)
   end
   let!(:employee1) do
-    FactoryBot.create(:employee, role_id: role.id, designation_id: designation.id, date_of_birth: Time.now.to_date)
+    FactoryBot.create(:employee, role_id: role.id, designation_id: designation.id, date_of_birth: Time.now.to_date,
+                                 joining_date: Time.now.to_date)
   end
 
   describe 'index' do
@@ -20,6 +21,11 @@ RSpec.describe DashboardController, type: :controller do
     it 'To show the details of those employees whose birthday is today ' do
       get :index
       expect(assigns(:employees).count).to eq(2)
+    end
+
+    it 'To show the details of those employees whose Recent withing 3 days joined' do
+      get :index
+      expect(assigns(:recent_join_employees).count).to eq(1)
     end
   end
 end
