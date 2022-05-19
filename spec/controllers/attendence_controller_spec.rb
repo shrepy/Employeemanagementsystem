87504904b5d0b7edb827d960f956e6491ef3ccf8
ubuntu <you@example.com>
2022-05-19 16:20:07 +0530

    Employee Attendence Module

--- spec/controllers/attendence_controller_spec.rb
+++ spec/controllers/attendence_controller_spec.rb
@@ -3,23 +3,46 @@
 require 'rails_helper'
 
 RSpec.describe AttendencesController, type: :controller do
-  # describe 'update' do
-  #   it 'checkout' do
-  #     employee = FactoryBot.create(:employee)
-  #     attendence = FactoryBot.create(:attendence, employee_id: employee.id)
-  #     patch :update, params: { id: attendence.id, checkout_time: Time.now }
-  #     expect(response.status).to eq(302)
-  #   end
-  # end
+  fixtures :all
 
-  # describe 'create' do
-  #   employee = FactoryBot.create(:employee)
-  #   before do
-  #     sign_in(employee)
-  #   end
-  #   it 'checkin' do
-  #     post :create, params: { employee_id: employee.id }
-  #     expect(response.status).to eq(302)
-  #   end
-  # end
+  let!(:designation) { FactoryBot.create(:designation) }
+  let!(:employee) { FactoryBot.create(:employee, role_id: roles(:role_one).id, designation_id: designation.id) }
+  let!(:attendence) { FactoryBot.create(:attendence, employee_id: employee.id) }
+  before do
+    sign_in(employee)
+  end
+
+  describe 'index' do
+    it 'Get Attendence' do
+      get :index
+      expect(response.status).to eq(200)
+    end
+
+    context 'Show Attendence' do
+      it 'renders a successful response when Attendence exist' do
+        get :show, params: { id: attendence.id }
+        expect(assigns(:attendence)).to eq(attendence)
+      end
+
+      it 'redirect to root path if Attendence is not exist' do
+        get :show, params: { id: '' }
+        assert_response(302)
+      end
+    end
+  end
+
+  describe 'create' do
+    it 'checkin' do
+      post :create, params: { employee_id: employee.id }
+      expect(response.status).to eq(302)
+    end
+  end
+
+  describe 'update attendence' do
+    let!(:attendence) { FactoryBot.create(:attendence, employee_id: employee.id, checkin_time: Time.now) }
+    it 'checkout by employee' do
+      patch :update_attendence
+      expect(response.status).to eq(302)
+    end
+  end
 end
