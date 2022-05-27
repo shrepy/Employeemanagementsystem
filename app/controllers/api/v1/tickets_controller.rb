24aa4972dac5504c90be123c61e61eed147d0028
Udaysass <udayv.sass@gmail.com>
2022-05-27 12:55:42 +0530

    changes in rspec

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -8,38 +8,38 @@ module Api
       skip_before_action :verify_authenticity_token
 
       def index
-        data = Ticket.where(employee_id: current_employee)
+        ticket_data = Ticket.where(employee_id: current_employee)
         render json: {
-          data: serializer_data(data, serializer),
+          data: serializer_data(ticket_data, serializer),
           message: ['show tickets '], status: 200, type: 'Success'
         }
       end
 
       def create
-        data = Ticket.new(ticket_params)
-        if data.save
+        ticket_data = Ticket.new(ticket_params)
+        if ticket_data.save
           render json: {
-            data: serializer_data(data, serializer),
+            data: serializer_data(ticket_data, serializer),
             message: ['Successfully ticket create '], status: 200, type: 'Success'
           }
         else
-          render json: data
+          render json: ticket_data, message: ['leave status not update'], status: 304, type: 'failed'
         end
       end
 
       def update
-        data = Ticket.find(params[:id])
-        if data.status.nil?
-          if data.update(status: 'accept', updated_by: current_employee.name)
+        ticket_data = Ticket.find(params[:id])
+        if ticket_data.status.nil?
+          if ticket_data.update(status: 'accept', updated_by: current_employee.name)
             render json: {
-              data: serializer_data(data, serializer),
-              message: ['Successfully daily task create '], status: 200, type: 'Success'
+              data: serializer_data(ticket_data, serializer),
+              message: ['Successfully ticket create '], status: 200, type: 'Success'
             }
           else
             render :index
           end
         else
-          render json: data
+          render json: ticket_data
         end
       end
 
--- spec/v1/ticket_spec.rb
+++ spec/v1/ticket_spec.rb
@@ -6,31 +6,39 @@ describe Api::V1::TicketsController, type: :controller do
   let(:role) { FactoryBot.create(:role) }
   let(:designation) { FactoryBot.create(:designation) }
   let(:employee) { FactoryBot.create(:employee, designation_id: designation.id, role_id: role.id) }
-  let(:ticket) { FactoryBot.create(:ticket, employee_id: employee.id) }
+  let!(:ticket) { FactoryBot.create(:ticket, employee_id: employee.id) }
 
   before do
     sign_in(employee)
   end
 
   describe '#index' do
-    it 'show all Tickets ' do
-      get :index
-      expect(response.status).to eq(200)
+    it 'show all ticket ' do
+      get :index, params: { format: :json }
+      response_body = JSON.parse(response.body)
+      expect(response_body['data'][0]['id']).to eq(ticket.id)
     end
   end
 
+  let(:valid_ticket) { attributes_for(:ticket, employee_id: employee.id) }
+
   describe '#create' do
-    it 'create a Ticket' do
-      post :create, params: { ticket: { employee_id: employee.id } }
+    it 'renders a successful response when ticket created' do
+      post :create, params: { ticket: valid_ticket }
+      expect(Ticket.count).to eq(2)
+      response_body = JSON.parse(response.body)
+      expect(response_body['message']).to eq(['Successfully ticket create '])
       expect(response.status).to eq(200)
     end
   end
 
   describe '#update' do
-    it 'render to update' do
+    it 'renders a successful response when ticket status update' do
       patch :update,
             params: { ticket: { status: 'accept', employee_id: employee.id }, id: ticket.id }
-      assert_response(200)
+      response_body = JSON.parse(response.body).deep_symbolize_keys
+      expect(response_body[:data][:status]).to eq('accept')
+      expect(response.status).to eq(200)
     end
   end
 end
