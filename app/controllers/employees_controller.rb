24e4791ce893676d22ebd9c462f322b1c2dcb5a3
ubuntu <you@example.com>
2022-05-30 20:04:01 +0530

    Add Enployee gender

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,8 +3,8 @@
 # Employee controller
 class EmployeesController < ApplicationController
   before_action :authenticate_employee!
+  before_action :set_employee, only: %i[show, update]
   load_and_authorize_resource
-  #before_action :set_employee, only: %i[show, edit]
 
   # GET /employees or /employees.json
   def index
@@ -14,29 +14,22 @@ class EmployeesController < ApplicationController
 
   # GET /employees/1 or /employees/1.json
   def show
-    if current_user.role.name == 'HR'
-      @employee = Employee.find(params[:id])
-    else
-      @employee = current_user
-    end
+    @employee = if current_user.is_hr?
+                  Employee.find_by_id params[:id]
+                else
+                  current_user
+                end
   end
 
   def edit 
-    @designations = Designation.all 
-    @skills = Skill.all
-    @roles = Role.all
+    set_designations
   end
 
   def update
-    @designations = Designation.all 
-    @skills = Skill.all
-    @roles = Role.all
-    @employee = Employee.find(params[:id])
+    @employee = Employee.find_by_id params[:id]
     if @employee.update(employee_params)
       redirect_to @employee
     else
-      #render js: edit, status: :unprocessable_entity
-      #render json: :edit
       render :edit
     end
   end
@@ -48,14 +41,19 @@ class EmployeesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
+  def set_designations
+    @designations = Designation.all 
+    @skills = Skill.all
+    @roles = Role.all
+  end
+
   def set_employee
-    @employee = Employee.find(params[:id])
-    render :show, notice: 'Employee not found', status: 404 unless @employee.present?
+    @employee = Employee.find_by_id params[:id]
+    redirect_to root_path,  alert: I18n.t('employee.not_found') unless @employee.present?
   end
 
   def employee_params
-     params.require(:employee).permit(:name, :father_name, :mother_name, :phone_number, :address, :email, :bank_name, :account_number, :pan_card_number, :aadhar_card_number,:primary_skill, :password, :password_confirmation, :designation_id, :role_id, :image, :date_of_birth, :joining_date)
+     params.require(:employee).permit(:name, :father_name, :mother_name, :phone_number, :address, :email, :bank_name, :account_number, :pan_card_number, :aadhar_card_number,:primary_skill, :password, :password_confirmation, :designation_id, :role_id, :image, :date_of_birth, :joining_date, :gender)
   end
 end
 
--- spec/controllers/employee_controller_spec.rb
+++ spec/controllers/employee_controller_spec.rb
@@ -3,29 +3,59 @@
 require 'rails_helper'
 
 RSpec.describe EmployeesController, type: :controller do
-  # context '#index' do
-  # let!(:role) { FactoryBot.create(:role )}
-  # let!(:employee) { FactoryBot.create(:employee, role_id:  role.id) }
-  # byebug
-  # describe 'index' do
-  #   before do
-  #     sign_in(employee)
-  #     get :index
-  #   end
+  fixtures :all
 
-  #   it 'Get Employee' do
-  #     get :index
-  #     expect(response.status).to eq(200)
-  #   end
+  let!(:designation) { FactoryBot.create(:designation) }
+  let!(:role) { FactoryBot.create(:role) }
+  let!(:employee) { FactoryBot.create(:employee, role_id: roles(:role_one).id, designation_id: designation.id) }
+  let!(:employee_one) { FactoryBot.create(:employee, role_id: roles(:role_one).id, designation_id: designation.id) }
+  before do
+    sign_in(employee)
+  end
 
-  #   it 'Show Employee' do
-  #     get :show, params: { id: employee.id }
-  #     expect(assigns(:employee)).to eq(employee)
-  #   end
+  describe '#index' do
+    before do
+      sign_in(employee)
+    end
+    it 'Get Employee' do
+      get :index
+      expect(assigns(:employees).count).to eq(2)
+    end
+  end
 
-  #   it 'When employee not exist' do
-  #     get :show, params: { id: employee.id + 1 }
-  #     expect(response.status).to eq(404)
-  #   end
-  # end
+  describe '#show' do
+    it 'Show Employee' do
+      get :show, params: { id: employee.id }
+      expect(assigns(:employee)).to eq(employee)
+    end
+
+    it 'return nil when employee not exist' do
+      get :show, params: { id: '' }
+      expect(assigns(:employee)).to eq(nil)
+    end
+  end
+
+  describe '#edit' do
+    it 'response successfully when employee data is exist' do
+      get :edit, params: { id: employee.id }
+      expect(response.status).to eq(200)
+    end
+
+    it 'return nil when employee not exist' do
+      get :edit, params: { id: '' }
+      expect(assigns(:employee)).to eq(nil)
+    end
+  end
+
+  describe '#update' do
+    it 'response successfully when employee data is updated' do
+      patch :update, params: { employee: { name: 'xyz' }, id: employee.id }
+      expect(assigns(:employee)).to eq(employee)
+    end
+
+    it 'return nil when employee not exist' do
+      patch :update, params: { employee: { name: 'Xyz' }, id: '' }
+      expect(assigns(:employee)).to eq(nil)
+    end
+  end
 end
