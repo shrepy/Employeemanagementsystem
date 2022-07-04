c23e54ae78352c63c4fef5e459a41efe4a90fb5b
riya <riyas.sass4@gmail.com>
2022-07-04 18:36:35 +0900

    optimize code

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,23 +1,28 @@
-module TicketsHelper
-  def accept_decline(ticket)
-    if current_employee.is_admin?
+# frozen_string_literal: true
 
-      case ticket.status
-      when 'accept'
-        accept = button_tag 'Accepted', class: 'btn btn-success  disabled'
-      when 'decline'
-       decline = button_tag 'Declined', class: 'btn btn-danger disabled'
-      else
-        accept = link_to 'Accept', admin_main_ticket_path(ticket, status: 'accept'), method: :patch, class: 'btn btn-success'
-        decline = link_to 'Decline', admin_main_ticket_path(ticket, status: 'decline'), method: :patch, class: 'btn btn-danger'
-      end
-    elsif ticket.status.nil?
+module TicketsHelper
+  def admin_status(ticket)
+    case ticket.status
+    when 'accept'
+      accept = 'Accepted'
+    when 'decline'
+      decline = 'Declined'
+    else
+      accept = link_to 'Accept', admin_main_ticket_path(ticket, status: 'accept'), method: :patch,
+                                                                                   class: 'btn btn-success'
+      decline = link_to 'Decline', admin_main_ticket_path(ticket, status: 'decline'), method: :patch,
+                                                                                      class: 'btn btn-danger'
+    end
+    [accept, decline]
+  end
+
+  def accept_decline(ticket)
+    if ticket.status.nil?
       Pending
     elsif ticket.status == 'accept'
       Accepted
     else
       Rejected
     end
-    [accept, decline]
-  end 
+  end
 end
