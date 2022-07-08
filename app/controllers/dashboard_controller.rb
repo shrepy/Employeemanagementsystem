ff56993ef1f0c06a906a44f3cf6637904b6f2405
riya <riyas.sass4@gmail.com>
2022-07-08 18:57:37 +0530

    upcoming holiday

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,18 +1,12 @@
 # frozen_string_literal: true
 
-# dashboard controller
 class DashboardController < ApplicationController
   before_action :authenticate_employee!
-  skip_before_action :authenticate_employee!, only: [:set_ip]
   def index
     @employees = Employee.where('EXTRACT(DAY FROM date_of_birth) = ?', Date.current.day).where(
       'EXTRACT(MONTH FROM date_of_birth) = ?', Date.current.month
     )
     @recent_join_employees = Employee.where(joining_date: (Time.now.to_date - 2.day)..Time.now.to_date)
+    @upcoming_holidays = Holiday.where(holiday_date: (Time.now.to_date)..Time.now.to_date + 4.day)
   end
-
-  # def set_ip
-  #   @ips = Ip.create(unblockip: params[:ip])
-  #   redirect_to root_path if @ips.save
-  # end
 end
