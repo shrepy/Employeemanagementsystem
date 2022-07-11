b64a0f867f4dfb8dc8ba608eb421547e58d90d7b
riya <riyas.sass4@gmail.com>
2022-07-11 16:49:38 +0530

    upcoming holiday

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# dashboard controller
 class DashboardController < ApplicationController
   before_action :authenticate_employee!
   def index
@@ -7,6 +8,6 @@ class DashboardController < ApplicationController
       'EXTRACT(MONTH FROM date_of_birth) = ?', Date.current.month
     )
     @recent_join_employees = Employee.where(joining_date: (Time.now.to_date - 2.day)..Time.now.to_date)
-    @upcoming_holidays = Holiday.where(holiday_date: (Time.now.to_date)..Time.now.to_date + 4.day)
+    @upcoming_holidays = Holiday.where('holiday_date > ?', Time.now).limit(5).order('holiday_date ASC')
   end
 end
