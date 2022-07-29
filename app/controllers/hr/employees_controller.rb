84dc57e141e033f76c677118ee5ba67cdb311821
Namrata12gupta <namratag.sass@gmail.com>
2022-07-29 16:47:00 +0530

    add mannual button for leave in HR functinality

--- app/controllers/hr/employees_controller.rb
+++ app/controllers/hr/employees_controller.rb
@@ -1,34 +1,45 @@
-class Hr::EmployeesController < ApplicationController
-    #before_action :set_employee, only: %i[new, create]
+# frozen_string_literal: true
+
+module Hr
+  class EmployeesController < ApplicationController
     load_and_authorize_resource
 
     def index
-        redirect_to new_hr_employee_path
+      redirect_to new_hr_employee_path
     end
 
     def new
-        @employee = Employee.new
-        @designations = Designation.all 
-        @skills = Skill.all
-        @roles = Role.all   
+      @employee = Employee.new
+      @designations = Designation.all
+      @skills = Skill.all
+      @roles = Role.all
     end
 
     def create
-        @designations = Designation.all 
-        @skills = Skill.all
-        @roles = Role.all   
-        @employee = Employee.new(employee_params)
-        if @employee.save
-           redirect_to @employee
-        else 
-            render :new 
-        end
+      @designations = Designation.all
+      @skills = Skill.all
+      @roles = Role.all
+      @employee = Employee.new(employee_params)
+      if @employee.save
+        redirect_to @employee
+      else
+        render :new
+      end
     end
 
+    def update_leave_balance
+      leaves = Employee.all
+      leaves.each do |leave|
+        leave.increment!(:leave_count)
+      end
+      redirect_to root_path
+    end
 
     private
 
     def employee_params
-        params.require(:employee).permit(:designation_id, :role_id, :name, :father_name, :mother_name, :date_of_birth, :phone_number, :email, :address, :bank_name, :account_number, :pan_card_number, :aadhar_card_number, :primary_skill, :password, :password_confirmation, :joining_date, :gender, :image)
+      params.require(:employee).permit(:designation_id, :role_id, :name, :father_name, :mother_name, :date_of_birth,
+                                       :phone_number, :email, :address, :bank_name, :account_number, :pan_card_number, :aadhar_card_number, :primary_skill, :password, :password_confirmation, :joining_date, :gender, :image)
     end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
           get :index
         end
       end
+      get '/update_leave', to: 'employees#update_leave_balance', as: 'update_leave_balance'
     end
 
     resources :attendences
