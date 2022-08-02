1134eb6f2ce8ad4ed3b665309157d47c04a61cf0
Udaysass <udayv.sass@gmail.com>
2022-08-02 18:05:33 +0530

    formatting time and date

--- app/serializer/api/v1/attendence_serializer.rb
+++ app/serializer/api/v1/attendence_serializer.rb
@@ -4,6 +4,13 @@ module Api
   module V1
     class AttendenceSerializer < ActiveModel::Serializer
       attributes :id, :checkin_time, :checkout_time, :status, :hour
+      def checkin_time
+        object&.checkin_time&.strftime("%a %b %d %y %I:%M:%S %p")
+      end
+
+      def checkout_time
+        object&.checkout_time&.strftime("%a %b %d %y %I:%M:%S %p")
+      end
     end
   end
 end
