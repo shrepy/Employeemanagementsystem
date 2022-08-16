e12c8ab6a0558bd88e426c183b058402984fb295
Namrata12gupta <namratag.sass@gmail.com>
2022-08-16 18:43:56 +0530

    code optimize and right identation

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,16 +3,15 @@
 # Employee controller
 class EmployeesController < ApplicationController
   before_action :authenticate_employee!
-  before_action :set_employee, only: %i[show, update]
+  before_action :set_employee, only: %i[show update]
+  before_action :set_designations, only: %i[edit update]
   load_and_authorize_resource
 
-  # GET /employees or /employees.json
   def index
     @employees = Employee.search(params[:search])
     @designations = Designation.all
   end
 
-  # GET /employees/1 or /employees/1.json
   def show
     @employee = if current_user.is_hr?
                   Employee.find_by_id params[:id]
@@ -21,20 +20,15 @@ class EmployeesController < ApplicationController
                 end
   end
 
-  def edit 
-    set_designations
-  end
+  def edit; end
 
   def update
-    set_designations
-    @employee = Employee.find_by_id params[:id]
     if @employee.update(employee_params)
       redirect_to @employee
     else
       render :edit
     end
   end
-
 
   def profile
     @employee = Employee.all
@@ -43,18 +37,18 @@ class EmployeesController < ApplicationController
   private
 
   def set_designations
-    @designations = Designation.all 
+    @designations = Designation.all
     @skills = Skill.all
     @roles = Role.all
   end
 
   def set_employee
     @employee = Employee.find_by_id params[:id]
-    redirect_to root_path,  alert: I18n.t('employee.not_found') unless @employee.present?
+    redirect_to root_path, alert: I18n.t('employee.not_found') unless @employee.present?
   end
 
   def employee_params
-     params.require(:employee).permit(:name, :father_name, :mother_name, :phone_number, :address, :email, :bank_name, :account_number, :pan_card_number, :aadhar_card_number,:primary_skill, :password, :password_confirmation, :designation_id, :role_id, :image, :date_of_birth, :joining_date, :gender)
+    params.require(:employee).permit(:name, :father_name, :mother_name, :phone_number, :address, :email, :bank_name,
+                                     :account_number, :pan_card_number, :aadhar_card_number, :primary_skill, :password, :password_confirmation, :designation_id, :role_id, :image, :date_of_birth, :joining_date, :gender)
   end
 end
-
